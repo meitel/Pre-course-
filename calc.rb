6490019096cadf88339461025285a08a13c84786
Melanie Eitel <me@melanieeitel.com>
2013-07-20 12:27:17 -0400

    made some changes

--- calc.rb
+++ calc.rb
@@ -9,7 +9,11 @@ puts 5-8
 puts 9/2
 puts 5 * (12-8) + -15
 puts 98 + (59872 / (13*8)) * -51
+# hours in a year
 puts 365 * 24
+# minutes in a decade
 puts 60 * 24 * 365 * 10
 # my age in seconds
-puts (60*60*24*365*23)
+puts (60.0 * 60.0 * 24.0 * 365.0 * 23.0)
+# our dear authors age at 1160000000 seconds
+puts (1160000000.0 / 60.0 / 60.0 / 24.0 / 365.0)
