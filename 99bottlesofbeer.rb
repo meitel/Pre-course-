f97296375dc6d02d6dd6da9b376b71d2e5e242a8
Melanie Eitel <me@melanieeitel.com>
2013-07-28 15:35:11 -0400

    completed 99bottlesofbeer

--- 99bottlesofbeer.rb
+++ 99bottlesofbeer.rb
@@ -6,13 +6,14 @@ while bottle > 0
     puts bottle.to_s + ' Bottles of Beer on the Wall, ' + bottle.to_s + ' Bottles of Beer '
     puts 'Take One Down, Pass it Around'
     if bottle == 2
-      puts '1 Bottle of Beer on the Wall, 1 Bottle of Beer'
+      puts '1 Bottle of Beer on the Wall'
     end
     if  bottle > 2
       puts (bottle - 1).to_s + ' Bottles of Beer on the Wall '
     end 
   elsif bottle == 1
-    puts '1 Bottle of Beer'
+    puts '1 Bottle of Beer on the Wall, 1 Bottle of Beer'
+    puts 'Drinks the last one'
   elsif bottle == 0
     puts '1 Bottle of Beer'
   end
