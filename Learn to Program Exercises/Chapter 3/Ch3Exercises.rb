52284f510ef309d5191bdeddb4d7dc23590ef733
Melanie Eitel <me@melanieeitel.com>
2013-07-31 15:00:41 -0400

    Completed Chapter 3 Exercises

---
+++ Learn to Program Exercises/Chapter 3/Ch3Exercises.rb
@@ -0,0 +1,17 @@
+# 3.1 String Arithmetic
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+# 3.2 12 vs. '12'
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+# 3.3 Problems
+puts 'You\'re swell!'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
